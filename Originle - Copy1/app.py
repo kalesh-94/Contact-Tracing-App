f48ad2ae14787391e284b9b43eaf4f208c81ccf3
Kalesh Patil <159576620+kalesh-94@users.noreply.github.com>
2024-12-21 01:02:37 +0530

    Update app.py

--- Originle - Copy1/app.py
+++ Originle - Copy1/app.py
@@ -212,4 +212,6 @@ def update_status():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensure location is initialized for first-time run
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+ # Replace 5000 with your desired port
+
